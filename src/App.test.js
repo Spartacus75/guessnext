79b6cbb6ad035010674ffb5247f636594e1c0b27
Maanan <maanan.seduk@gmail.com>
2020-12-08 08:27:08 +0100

    Encore un test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('reners learn react link', () => {
+test('on change encore', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
