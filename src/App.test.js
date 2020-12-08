4a9159b39417e1940ad5e939a1cfe2d994c96194
Maanan <maanan.seduk@gmail.com>
2020-12-08 08:23:18 +0100

    Commit sur un fichier non utilisé

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('ren ders learn react link', () => {
+test('reners learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
